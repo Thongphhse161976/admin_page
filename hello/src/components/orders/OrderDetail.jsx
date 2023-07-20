33f373d457b206b0ccd8a6d9ba33bde17058d15e
LAPTOP-HKUL1DBD\phamthong <cothongpham@gmail.com>
2023-07-20 22:49:40 +0700

    header

--- hello/src/components/orders/OrderDetail.jsx
+++ hello/src/components/orders/OrderDetail.jsx
@@ -33,11 +33,14 @@ const OrderDetail = () => {
                         <Header />
                         <div class="container-fluid">
                             {/* Page Heading */}
-                            <h1 class="h3 mb-2 text-gray-800">Order Detail</h1>
+                            
                             {/* DataTales Example */}
                             <div class="card shadow mb-4">
                                 <div class="card-body">
                                     <div class="table-responsive">
+                                        <p className="text-center">
+                                            <h1 class="h3 mb-2 text-gray-800">Order Detail</h1>
+                                        </p>
                                         {orderDetail && orderDetail.orderDetails && <div className="col-sm-6 offset-sm-3">
                                             <span>Order ID :</span>
                                             <span>{orderDetail.id}</span>
